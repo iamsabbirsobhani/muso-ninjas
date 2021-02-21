84b00cd3e34219b53e1586de1d5a676fe0ddb87f
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-02-21 15:36:01 +0600

    Updated: Firebase config.js setup and connect with firebase backend

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeofjbtaLB25iy4_gmPgj3f5_1xuXiALI",
+    authDomain: "muso-ninjas-def94.firebaseapp.com",
+    projectId: "muso-ninjas-def94",
+    storageBucket: "muso-ninjas-def94.appspot.com",
+    messagingSenderId: "915811340580",
+    appId: "1:915811340580:web:4e67173177ef987ab7a3b1"
+  };
+// initialize firestore
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp}
