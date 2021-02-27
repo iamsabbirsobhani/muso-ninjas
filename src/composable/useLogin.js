1946231c314b4f925615169fa850bf843655b5d9
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-02-27 10:38:04 +0600

    Updated: Adding a 'pending' State to Requests
    Also added live chats composable files to here

--- src/composable/useLogin.js
+++ src/composable/useLogin.js
@@ -2,24 +2,28 @@ import { ref } from 'vue'
 import { projectAuth } from '../firebase/config'
 
 const error = ref(null)
+const isPending = ref(false)
 
 const login = async ( email, password ) => {
     error.value = null
+    isPending.value = true
 
     try{
         const res = await projectAuth.signInWithEmailAndPassword(email, password)
         error.value = null
+        isPending.value = false
 
         return res
     } catch(err) {
         let ermsg = err.message
         error.value = 'Incorrect login credentials'
         console.log(err.code)
+        isPending.value = false
     }
 }
 
 const userLogin = () => {
-    return {login, error}
+    return {login, error, isPending}
 }
 
 export default userLogin
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -4,16 +4,17 @@
     <input type="email" placeholder="Email" v-model="email" required />
     <input type="password" placeholder="Password" v-model="password" required />
     <div v-if="error" class="error"> {{ error }} </div>
-    <button class="btn">Login</button>
+    <button class="btn" v-if="!isPending">Login</button>
+    <button class="btn" v-if="isPending" disabled>loading</button>
   </form>
 </template>
 
 <script>
 import useLogin from "@/composable/useLogin";
-import { ref, watch } from "vue";
+import { ref } from "vue";
 export default {
   setup() {
-    const { login, error } = useLogin();
+    const { login, error, isPending } = useLogin();
 
     const email = ref("");
     const password = ref("");
@@ -25,7 +26,7 @@ export default {
       }
     };
 
-    return { email, password, handleSubmit, error };
+    return { email, password, handleSubmit, error, isPending };
   },
 };
 </script>
