2fc4ca364c46d09ed56e68d1bfc91b6735423309
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-03-02 22:54:44 +0600

    Updated: Conditionally Showing Links and Logout button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,63 +1,68 @@
 <template>
   <div class="navbar">
-      <nav>
-          <img src="@/assets/ninja.png" alt="">
-          <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link> </h1>
-          <div class="links">
-              <button @click="handleSubmit">Logout</button>
-              <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
-              <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
-          </div>
-      </nav>
+    <nav>
+      <img src="@/assets/ninja.png" alt="" />
+      <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <div v-if="user">
+          <button @click="handleSubmit">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
-import useLogout from '../composable/useLogout'
-import { useRouter } from 'vue-router'
+import useLogout from "../composable/useLogout";
+import getUser from "../composable/getUser";
+import { useRouter } from "vue-router";
 export default {
-    setup(){
-        const { logout, error} = useLogout()
-        const router = useRouter()
+  setup() {
+    const { user } = getUser();
+    const { logout } = useLogout();
+    const router = useRouter();
 
-        const handleSubmit = async () => {
-            await logout()
-            if(!error.value){
-                console.log('User Logged out')
-            }
-            router.push({ name: 'Login' })
-        }
+    const handleSubmit = async () => {
+      await logout();
+      console.log("User Logged out");
+      router.push({ name: "Login" });
+    };
 
-        return { handleSubmit }
-    }
-}
+    return { handleSubmit, user };
+  },
+};
 </script>
 
 <style scoped>
-    .navbar{
-        padding: 16px 10px;
-        margin-bottom: 60px;
-        background: white;
-    }
-    nav{
-        display: flex;
-        align-items: center;
-        max-width: 1200px;
-        margin: 0 auto;
-    }
-    nav h1{
-        margin-left: 20px;
-    }
-    nav .links{
-        /* New Tricks: On display flex, if its item set margin-left: auto (instead of justify-content: space-between)
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  /* New Tricks: On display flex, if its item set margin-left: auto (instead of justify-content: space-between)
          as auto it will be positioned auto left */
-        margin-left: auto;
-    }
-    nav .links a, button{
-        margin-left: 16px;
-        font-size: 14px;
-    }
-    nav img{
-        max-height: 60px;
-    }
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
 </style>
