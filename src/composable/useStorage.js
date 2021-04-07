0228c83c52e6971c484f41ae3d9be121d959b2ab
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-04-07 18:21:01 +0600

    adding new playlist

--- src/composable/useStorage.js
+++ src/composable/useStorage.js
@@ -7,22 +7,22 @@ const { user } = getUser()
 const useStorage = () => {
     const error = ref(null)
     const url = ref(null)
-    const filepath = ref(null)
+    const filePath = ref(null)
 
     const uploadImage = async (file) => {
-        filepath.value = `covers/${user.value.uid}/${file.name}`
-        const storageRef = projectStorage.ref(filepath.value)
+        filePath.value = `covers/${user.value.uid}/${file.name}`
+        const storageRef = projectStorage.ref(filePath.value)
 
         try{
             const res = await storageRef.put(file)
-            url.value = res.ref.getDownloadURL()
+            url.value = await res.ref.getDownloadURL()
         } catch(err) {
             console.log(err.message)
             error.value = err.message
         }
     }
+    return { url, filePath, error, uploadImage}
 
-    return { url, filepath, error, uploadImage}
 }
 
 export default useStorage
--- src/views/playlist/CreatePlaylist.vue
+++ src/views/playlist/CreatePlaylist.vue
@@ -9,53 +9,85 @@
     ></textarea>
     <!-- upload playlist image -->
     <label>Upload Playlist cover image</label>
-    <input type="file" @change="handleChange"/>
+    <input type="file" @change="handleChange" />
 
     <div class="error">{{ fileError }}</div>
 
     <div class="error"></div>
 
-    <button>Create</button>
+    <button v-if="!isPending">Create</button>
+    <button v-else disabled>Sending...</button>
   </form>
-  <img src="url" alt="">
+  <img src="url" alt="" />
 </template>
 
 <script>
-import useStorage from '@/composable/useStorage'
+import useStorage from "@/composable/useStorage";
 import { ref } from "vue";
+import useCollection from "@/composable/useCollection";
+import getUser from "@/composable/getUser";
+import { timestamp } from "../../firebase/config";
 export default {
   setup() {
-    const { filePath, url, uploadImage } = useStorage()
+    const { filePath, url, uploadImage } = useStorage();
+    const { error, addDoc } = useCollection("playlists");
+    const { user } = getUser();
 
     const title = ref("");
     const description = ref("");
-    const file = ref("")
-    const fileError = ref("")
+    const file = ref("");
+    const fileError = ref("");
+    const isPending = ref(false);
 
     const handleSubmit = async () => {
-      if(file.value) {
-        await uploadImage(file.value)
-        console.log('image uploaded, url: ', url.value)
+      if (file.value) {
+
+        // isPending start
+        isPending.value = true;
+        // end of isPending start
+
+        await uploadImage(file.value);
+
+        await addDoc({
+          title: title.value,
+          description: description.value,
+          userId: user.value.uid,
+          userName: user.value.displayName,
+          coverUrl: url.value,
+          filePath: filePath.value,
+          songs: [],
+          createdAt: timestamp(),
+        });
+
+        // isPending end
+        isPending.value = false;
+        // end of isPending end
+
+        if (!error.value) {
+          console.log("Playlist created");
+        }
+
+        console.log("image uploaded, url: ", url.value);
       }
     };
 
     const handleChange = (e) => {
-      const selected = e.target.files[0]
+      const selected = e.target.files[0];
 
       // allowed file types
-      const fileType = ['image/png', 'image/jpeg']
+      const fileType = ["image/png", "image/jpeg"];
 
-      if(selected && fileType.includes(selected.type)){
-        file.value = selected
-        console.log(file.value)
-        fileError.value = null
+      if (selected && fileType.includes(selected.type)) {
+        file.value = selected;
+        console.log(file.value);
+        fileError.value = null;
       } else {
-        file.value = null
-        fileError.value = "Please select an image file (png or jpeg)"
+        file.value = null;
+        fileError.value = "Please select an image file (png or jpeg)";
       }
-    }
+    };
 
-    return { title, description, handleSubmit, handleChange, fileError, url };
+    return { title, description, handleSubmit, handleChange, fileError, url, isPending };
   },
 };
 </script>
